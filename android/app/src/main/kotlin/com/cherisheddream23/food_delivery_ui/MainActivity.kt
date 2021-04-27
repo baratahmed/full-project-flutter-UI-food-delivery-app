99161bad22d9e5665f873a66b1a10d4d3d54af62
baratahmed <baratahmed28@gmail.com>
2021-04-28 01:56:15 +0600

    Full UI Set Up Done

---
+++ android/app/src/main/kotlin/com/cherisheddream23/food_delivery_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cherisheddream23.food_delivery_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/food.dart
@@ -0,0 +1,6 @@
+class Food {
+  final String imageUrl;
+  final String name;
+  final double price;
+  const Food({this.imageUrl, this.name, this.price});
+}
